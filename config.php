58c3e5d5887256dfbde086001dc9b8253f090b84
0xBinaryBits <ashinabdulla2018@gmail.com>
2024-08-03 11:06:28 +0530

    Updated config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 return[
             "database" => 
            [ 
-            "host"=>"ip",
-            "port"=>port,
-            "dbname"=>"databse_name",
+            "host"=>"127.0.0.1",
+            "port"=>3306,
+            "dbname"=>"myapp",
             "charset"=>"utf8mb4"
             ]
         ];
